8c74f7e31767557e21732accad38002afd69f3b9
neil <neil@ghtinc.com>
2016-07-15 15:29:03 +0800

    broker prevents events stack too deep
    and expose broker when in development

--- app/broker.js
+++ app/broker.js
@@ -1,12 +1,30 @@
 // our central events broker
 import EventEmitter from 'events';
 
+const debug = require('debug')('app/broker');
+
 class Broker extends EventEmitter {
+  constructor () {
+    super();
+    this.count = 0;
+  }
   do (action) {
-    this.emit('do', action);
+    this.count ++;
+    if(this.count > 1000) {
+      debug(`WARNING: events stack over ${this.count}`);
+      requestAnimationFrame(() => {
+        this.emit('do', action);
+      });
+    } else  {
+      this.emit('do', action);
+    }
+    this.count --;
   }
 }
 
-const b = new Broker();
+const B = new Broker();
+export default B;
 
-module.exports = b;
+if(module.hot) {
+  window.B = B;
+}
